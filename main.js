59236886b5d500a61e2f3bfb2f6f5a0a68ef6aaf
Sriharish-947 <74801176+Sriharish-947@users.noreply.github.com>
2021-11-05 18:58:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup()
+{
+    createCanvas(400, 400);
+    canvas.center();
+    
+}
+
+function preload()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function takeSnapshot()
+{
+    //code for taking snapshot
+    
+}
